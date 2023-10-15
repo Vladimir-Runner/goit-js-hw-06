d1c0a6da2075778c52c99085dd3f4e0ff0dbdff6
Vladimir-Runner <volodimir.sokolovskiy@gmail.com>
2023-10-16 02:22:04 +0300

    3

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,25 @@
-const title = document.querySelector("li");
-console.log(title);
+const categoryEl = document.querySelectorAll("li.item");
+console.log(`Number of categories: ${categoryEl.length}`);
+
+let arrayElements = [];
+categoryEl.forEach((elem) => {
+  arrayElements.push(elem.children);
+});
+
+let arrTitles = [];
+arrayElements.forEach((element) => {
+  arrTitles.push(element[0].textContent);
+});
+
+let arrCountElem = [];
+arrayElements.forEach((el) => {
+  arrCountElem.push(el[1].children.length);
+});
+
+function makeDescription(category, counter) {
+  for (let i = 0; i < arrTitles.length; i += 1) {
+    console.log(`Category: ${arrTitles[i]}`);
+    console.log(`Elements: ${arrCountElem[i]}`);
+  }
+}
+makeDescription(arrTitles, arrCountElem);
