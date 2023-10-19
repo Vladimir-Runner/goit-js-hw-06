3d2f4bb189da8cff31cde9ca0f09d2736257e86a
Vladimir-Runner <135231554+Vladimir-Runner@users.noreply.github.com>
2023-10-20 00:55:36 +0300

    js-5

--- js/task-05.js
+++ js/task-05.js
@@ -1,12 +1,25 @@
-const textInput = document.querySelector("#name-input");
-const textOutput = document.querySelector("#name-output");
+// const textInput = document.querySelector("#name-input");
+// const textOutput = document.querySelector("#name-output");
 
-textInput.addEventListener("input", updateInput);
+// textInput.addEventListener("input", updateInput);
 
-function updateInput(event) {
+// function updateInput(event) {
+//   if (event.currentTarget.value.trim() === "") {
+//     textOutput.textContent = "Anonymous";
+//   } else {
+//     textOutput.textContent = event.currentTarget.value;
+//   }
+// }
+
+const input = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
+
+input.addEventListener("input", onChangeInput);
+
+function onChangeInput(event) {
   if (event.currentTarget.value.trim() === "") {
-    textOutput.textContent = "Anonymous";
+    output.textContent = "Anonymous";
   } else {
-    textOutput.textContent = event.currentTarget.value;
+    output.textContent = event.currentTarget.value;
   }
 }
