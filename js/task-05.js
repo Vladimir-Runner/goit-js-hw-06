2df9807832ff7439cadb116066fd12f215666611
Vladimir-Runner <135231554+Vladimir-Runner@users.noreply.github.com>
2023-10-20 00:50:22 +0300

    js-5

--- js/task-05.js
+++ js/task-05.js
@@ -1,12 +1,12 @@
 const textInput = document.querySelector("#name-input");
 const textOutput = document.querySelector("#name-output");
 
-input.addEventListener("input", updateInput);
+textInput.addEventListener("input", updateInput);
 
 function updateInput(event) {
   if (event.currentTarget.value.trim() === "") {
-    output.textContent = "Anonymous";
+    textOutput.textContent = "Anonymous";
   } else {
-    output.textContent = event.currentTarget.value;
+    textOutput.textContent = event.currentTarget.value;
   }
 }
