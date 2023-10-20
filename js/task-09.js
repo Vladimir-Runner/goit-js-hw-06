3de65e678383522a45dcffddbce09184cde90013
Vladimir-Runner <135231554+Vladimir-Runner@users.noreply.github.com>
2023-10-20 19:18:00 +0300

    js-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
+const btn = document.querySelector(".change-color");
+const color = document.querySelector(".color");
+const body = document.querySelector("body");
+
+btn.addEventListener("click", changeColor);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+function changeColor() {
+  const colorClick = getRandomHexColor();
+  color.textContent = colorClick;
+  body.style.backgroundColor = colorClick;
 }
